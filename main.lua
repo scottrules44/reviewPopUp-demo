3fbe7154048bc6df630b3805af221b26309640e2
Scott H Tech <scottrules44@gmail.com>
2020-08-30 20:07:08 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+--Old verison (will work for android but not iOS)
+--[[
 local platformVersion = system.getInfo( "platformVersion" ) or 0
 local iOSVersion = tonumber(string.sub( platformVersion, 1, 4 )) 
 if( iOSVersion >= 10.3 and system.getInfo("platform") == "ios" ) then 
@@ -6,3 +8,11 @@ if( iOSVersion >= 10.3 and system.getInfo("platform") == "ios" ) then
 else
 	local textAlert = display.newText( "You need iOS 10.3 to test", display.contentCenterX, display.contentCenterY, native.systemFont, 20 )
 end
+]]--
+local reviewPopUp = require "plugin.reviewPopUp"
+reviewPopUp.init(function (e)
+	print(e.status)
+	if e.status == "ready" then
+		reviewPopUp.show()
+	end
+end)
