fdf97be74cb56c2631ba82e13685247cda376503
Scott H Tech <scottrules44@gmail.com>
2020-10-27 15:03:38 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,9 +9,12 @@ else
 	local textAlert = display.newText( "You need iOS 10.3 to test", display.contentCenterX, display.contentCenterY, native.systemFont, 20 )
 end
 ]]--
+local bg = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
+bg:setFillColor(.5)
+local title = display.newText("Review PopUp Plugin", display.contentCenterX, 50, native.systemFontBold, 15)
 local reviewPopUp = require "plugin.reviewPopUp"
-reviewPopUp.init(function (e)
-	print(e.status)
+
+reviewPopUp.init(function(e)
 	if e.status == "ready" then
 		reviewPopUp.show()
 	end
