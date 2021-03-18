c81b959a7d1666ffade7af4549c769a922090e0a
Scott H Tech <scottrules44@gmail.com>
2021-03-18 17:45:50 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 --Old verison (will work for iOS but not Android
+--[[
 local platformVersion = system.getInfo( "platformVersion" ) or 0
 local iOSVersion = tonumber(string.sub( platformVersion, 1, 4 )) 
 if( iOSVersion >= 10.3 and system.getInfo("platform") == "ios" ) then 
